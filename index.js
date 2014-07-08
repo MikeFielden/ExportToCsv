69ff27bc871f7a5ad64b09a76a8ecbdb3b4d54f8
Mike Fielden <fielden.mike@gmail.com>
2014-07-07 23:03:39 -0500

    Stubbing out the idea

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 'use strict';
 
-var exportToCsv = function (options) {
+var _ = require('lodash');
 
+var defaultOptions = {
+	colDelim: '","',
+	rowDelim: '"\r\n"',
+	headerRow: true
+
+};
+
+var exportToCsv = function (options) {
+	this.options = _.defaults(options, defaultOptions);
 };
 
 module.exports = exportToCsv;
