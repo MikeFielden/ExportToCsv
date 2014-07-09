bc643d6f2e5c259eb692a13f514055f9b4267402
Mike Fielden <fielden.mike@gmail.com>
2014-07-09 06:16:45 -0500

    Idea number 2
    WIP

--- index.js
+++ index.js
@@ -1,16 +1,51 @@
 'use strict';
 
-var _ = require('lodash');
+(function (root, factory) {
+	'use strict';
+	if (typeof define == 'function' && define.amd) {
+		define(['underscore'], factory);
+		define(['jquery'], factory);
+	}
+	else {
+		root.ExportToCsv = factory(root._, root.$);
+	}
+}(this, function (_, $) {
+	var options = {};
+	
+	if (!_ || !$) {
+		throw new Error('Must have _ and $.');
+	}
 
-var defaultOptions = {
-	colDelim: '","',
-	rowDelim: '"\r\n"',
-	headerRow: true
+	var buildHeaderRow = function ($theads, csv) {
+		var headers = _.map($theads.toArray(), function (el) {
+							return el.innerHTML;
+						});
 
-};
+		headers.push(options.rowDelim);
 
-var exportToCsv = function (options) {
-	this.options = _.defaults(options, defaultOptions);
-};
+		csv.push(headers.join(options.colDelim));
+	};
 
-module.exports = exportToCsv;
+	var exportToCsv = function (selector, userOpts) {
+		var defaultOptions = {
+			colDelim: '","',
+			rowDelim: '"\r\n"',
+			headerRow: true
+		};
+		
+		var csv = [];
+
+		var $table = $(selector);
+		var $theads = $(selector + ' thead th');
+
+		options = _.defaults(userOpts || {}, defaultOptions);
+
+		if (options.headerRow) {
+			buildHeaderRow($theads, csv);
+		}
+
+		console.log(csv);
+	}
+
+	return exportToCsv;
+}));
