b8d9d9df1b17573b9805f79d7a18727db97509ad
Mike Fielden <fielden.mike@gmail.com>
2014-07-10 10:28:33 -0500

    Beginnings of testing

--- test.js
+++ test.js
@@ -2,5 +2,13 @@ var assert = require('assert');
 var tocsv = require('./index');
 
 describe('ExportToCsv', function () {
+	describe('export', function () {
+		it('should handle no options', function () {
+			console.log()
+			var c = new tocsv('table');
 
+			assert.isDefined(c);
+		});
+
+	});
 });
