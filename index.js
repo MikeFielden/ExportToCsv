11fbd5e603d51d148ffa028141c7cbc33cb1cb66
Mike Fielden <fielden.mike@gmail.com>
2014-07-10 09:40:50 -0500

    Add comments, compatibility improvements
    Should now be compatible with AMD, CommonJS, NodeJs, etc

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-'use strict';
-
-(function (root, factory) {
-	'use strict';
-	if (typeof define == 'function' && define.amd) {
-		define(['underscore'], factory);
-		define(['jquery'], factory);
-	}
-	else {
-		root.ExportToCsv = factory(root._, root.$);
-	}
-}(this, function (_, $) {
+/**
+ * Should be compatible with AMD, CommonJS, Nodejs and browsers (cross-browser)
+ *
+ * Author: Mike Fielden, @Mike_Fielden, fielden.mike@gmail.com
+ */
+(function (name, definition, context, dependencies) {
+  if (typeof context['module'] !== 'undefined' && context['module']['exports']) { if (dependencies && context['require']) { for (var i = 0; i < dependencies.length; i++) context[dependencies[i]] = context['require'](dependencies[i]); } context['module']['exports'] = definition.apply(context); }
+  else if (typeof context['define'] !== 'undefined' && context['define'] === 'function' && context['define']['amd']) { define(name, (dependencies || []), definition); }
+  else { context[name] = definition(); }
+})('exportToCsv', function () {
 	var options = {};
 
-	if (!_ || !$) {
-		throw new Error('Must have _ and $.');
-	}
-
+	/**
+	 * buildHeaderRow()
+	 * Builds the header of the csv
+	 * Alters the main csv array
+	 *
+	 * @param <jqObj> $theads
+	 * @param <Array> csv
+	 * @return nothing
+	 */
 	var buildHeaderRow = function ($theads, csv) {
 		var headers = _.map($theads.toArray(), function (el) {
 							return el.innerHTML;
@@ -26,6 +29,15 @@
 		csv.push(headers.join(options.colDelim));
 	};
 
+	/**
+	 * buildBody()
+	 * Builds the body of the csv
+	 * Alters the main csv array
+	 *
+	 * @param <jqObj> $trs
+	 * @param <Array> csv
+	 * @return <Array> newly concated csv
+	 */
 	var buildBody = function ($trs, csv) {
 		var rows = _.map($trs.toArray(), function($tr) {
 			var thing = _.map($tr.children, function ($td) {
@@ -40,21 +52,30 @@
 		return csv.concat(rows);
 	};
 
+	/**
+	 * exportToCsv()
+	 * Main function, does all the lifting
+	 *
+	 * @param <String> selector
+	 * @param <Object> userOpts
+	 * @return nothing
+	 */
 	var exportToCsv = function (selector, userOpts) {
 		var defaultOptions = {
 			colDelim: ',',
 			rowDelim: '\r\n',
 			headerRow: true,
-			fileName: 'data.csv'
+			fileName: 'data.csv',
+			autoDownload: true
 		};
 
 		var csv = [];
 		var a = document.createElement('a');
-		var blob, url;
-
 		var $table = $(selector);
 		var $theads = $(selector + ' thead th');
 		var $bodyRows = $(selector + ' tbody tr');
+
+		var blob, url;
 
 		if ($table.length === 0) {
 			throw new Error('Table not found with the selector "' + selector + '"');
@@ -86,7 +107,11 @@
 			window.URL.revokeObjectURL(url);
 			document.body.removeChild(a);
 		};
+
+		if (options.autoDownload) {
+			this.download();
+		}
 	}
 
 	return exportToCsv;
-}));
+}, this, ['jquery', '_']);
