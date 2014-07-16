32a0b8fe46980b7bc53477f07301b447f132bc5a
Fielden <mfielden@jssda40050804.ndc.nasa.gov>
2014-07-16 15:09:00 -0500

    Added a fixture to test the DOM

--- spec/exportToCsvSpec.js
+++ spec/exportToCsvSpec.js
@@ -1,29 +1,28 @@
+function setUpHtmlFixture() {
+	jasmine.getFixtures().set('<table></table>');
+}
+
 describe('Options tests', function () {
 	var csv;
 
 	beforeEach(function () {
+		setUpHtmlFixture();
+
 		csv = new exportToCsv('table', {autoDownload: false});
 	});
 
 	it('should be defined', function () {
 		expect(csv).toBeDefined();
 	});
+
+	it('should not autoDownload', function () {
+		spyOn(csv, "download");
+
+		csv.download();
+
+		expect(csv.download).toHaveBeenCalled();
+	});
 });
-
-// QUnit.test('Options tests', function(assert) {
-// 	var csv = new exportToCsv('table', {autoDownload: false});
-
-// 	assert.notEqual(csv, undefined);
-
-// 	assert.equal(typeof csv.download, "function");
-// });
-
-// QUnit.test('Error condition tests', function(assert) {
-// 	// Should throw an error bc the table wasnt found
-// 	assert.throws(function () {
-// 		var csv = new exportToCsv('.xzy', {autoDownload: false});
-// 	});
-// });
 
 // QUnit.test('Should accept plain array', function (assert) {
 // 	var csv = new exportToCsv([6,7,8,9, "Things", "Other things"], {autoDownload: false});
